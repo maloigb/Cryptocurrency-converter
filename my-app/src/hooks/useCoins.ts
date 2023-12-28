d7b86ba12165df6ca74ba05f4d8334fd7199822b
Daniil <maloigb@gmail.com>
2023-12-28 13:19:56 +0300

    useCoins hook refactor

--- my-app/src/hooks/useCoins.ts
+++ my-app/src/hooks/useCoins.ts
@@ -1,13 +1,51 @@
 import useSWR from 'swr';
 import postCoins from '@/services/postCoins';
+import { calculateAthState } from '@/system/utils';
+import { useMemo } from 'react';
 
+const LIMIT_COINS = 10;
+
+interface Coin {
+    name: string,
+    price: string,
+    availableSupply: number,
+    athMarketCap: string,
+    category: string,
+    athPrice: string
+    fromAth: string,
+    toAth: string,
+    
+}
 
 export const useCoins = () => {
-    const { data, error, isLoading } = useSWR(`https://tstapi.cryptorank.io/v0/coins/`, postCoins.getCoins);
+    const { data, error, isLoading } = useSWR(`https://tstapi.cryptorank.io/v0/coins/`, (url) => postCoins.getCoins(url, {
+        limit : LIMIT_COINS,
+    }));
+
+    const normalizedCoins: Coin[] = useMemo(() => {
+        const coins = data?.data?.data || [];
+        
+        return coins.map((coin)  => {
+            const price = `$ ${Math.round(coin.price.USD)}`;
+            const athMarketCap = `${coin.athMarketCap.USD}`;
+            const fromAth = `${calculateAthState(coin.athPrice.USD, coin.price.USD).fromAth} %`;
+            const toAth = `${calculateAthState(coin.athPrice.USD, coin.price.USD).toAth} %`;
+            const athPrice = `${coin.athPrice.USD}`;
+
+            return {
+                ...coin,
+                price,
+                athMarketCap,
+                fromAth,
+                toAth,
+                athPrice
+            }
+        })}, [data]);
 
     return {
-        coins: data?.data,
+        coins: normalizedCoins,
         isLoading,
-        isError: error
+        error,
+        normalizedCoins
     };
 };
